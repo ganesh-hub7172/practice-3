f0598a51be9e884bba87df49756c53428a3281a7
MUSHAM GANESH <mushamganeshnetha7172@gmail.com>
2026-06-28 08:41:53 +0530

    Time: 1 ms (75.14%) | Memory: 12.5 MB (29.03%) - LeetSync

---
+++ 389-find-the-difference/find-the-difference.py
@@ -0,0 +1,11 @@
+class Solution:
+    def findTheDifference(self, s, t):
+        ans = 0
+
+        for c in s:
+            ans ^= ord(c)
+
+        for c in t:
+            ans ^= ord(c)
+
+        return chr(ans)
