1f47747f828c47390f5d6f0a6176e798827c818f
MUSHAM GANESH <mushamganeshnetha7172@gmail.com>
2026-08-01 11:54:25 +0530

    Time: 3 ms (56.65%) | Memory: 12.6 MB (20.23%) - LeetSync

---
+++ 486-predict-the-winner/predict-the-winner.py
@@ -0,0 +1,18 @@
+class Solution:
+    def predictTheWinner(self, nums):
+        memo = {}
+
+        def dp(i, j):
+            if i == j:
+                return nums[i]
+
+            if (i, j) in memo:
+                return memo[(i, j)]
+
+            left = nums[i] - dp(i + 1, j)
+            right = nums[j] - dp(i, j - 1)
+
+            memo[(i, j)] = max(left, right)
+            return memo[(i, j)]
+
+        return dp(0, len(nums) - 1) >= 0
